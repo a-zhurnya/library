78fcb353571529db866541c8bda0191ac742f018
d-bogdanov <denx-b@mail.ru>
2015-01-22 00:12:26 +0200

    Init commit

---
+++ library.js
@@ -0,0 +1,162 @@
+/**
+ * Определяет ширину scrollBar'а документа
+ * @returns {number}
+ */
+function getScrollbarWidth() {
+    var outer = document.createElement("div");
+    outer.style.visibility = "hidden";
+    outer.style.width = "100px";
+    outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+
+    document.body.appendChild(outer);
+
+    var widthNoScroll = outer.offsetWidth;
+    // force scrollbars
+    outer.style.overflow = "scroll";
+
+    // add innerdiv
+    var inner = document.createElement("div");
+    inner.style.width = "100%";
+    outer.appendChild(inner);
+
+    var widthWithScroll = inner.offsetWidth;
+
+    // remove divs
+    outer.parentNode.removeChild(outer);
+
+    return widthNoScroll - widthWithScroll;
+}
+
+/**
+ * Функция генерирует случайное число
+ * @param min
+ * @param max
+ * @returns {*}
+ */
+function getRandomInt(min, max) {return Math.floor(Math.random() * (max - min + 1)) + min;}
+
+/**
+ * Функция клонирует объект
+ * @param a
+ * @returns {*}
+ */
+function cloneObject(a){if(null==a||"object"!=typeof a)return a;var c=a.constructor(),b;for(b in a)c[b]=cloneObject(a[b]);return c}
+
+/**
+ * Функция считает кол-во элементов объекта
+ * @param ob
+ * @returns {number}
+ */
+function countObject(ob) {var count=0;for (var i in ob) {count++;}return count}
+
+/**
+ * Аналог php - empty()
+ * Возвращает истину, если переданное значение - пусто
+ * @param a
+ * @returns {boolean}
+ */
+function empty(a){return""==a||null==a||0==a||"0"==a||!1==a?!0:!1}
+
+/**
+ * Аналог php str_replace()
+ * Заменяет подстроку в строке на переданное значение
+ * Возвращает новую строку
+ * @param b
+ * @param a
+ * @param c
+ * @returns {*}
+ */
+function str_replace(b,a,c){if(!(a instanceof Array)&&(a=Array(a),b instanceof Array))for(;b.length>a.length;)a[a.length]=a[0];for(b instanceof Array||(b=Array(b));b.length>a.length;)a[a.length]="";if(c instanceof Array){for(d in c)c[d]=str_replace(b,a,c[d]);return c}for(var d=0;d<b.length;d++)for(var e=c.indexOf(b[d]);-1<e;)c=c.replace(b[d],a[d]),e=c.indexOf(b[d],e);return c}
+
+/**
+ * $('input').focus().selectRange(0,5);
+ * Поставить фокус в поле и выделить первые 5 символов
+ * @param start
+ * @param end
+ * @returns {*}
+ */
+$.fn.selectRange = function(start, end) {
+    return this.each(function() {
+        if (this.setSelectionRange) {
+            this.focus();
+            this.setSelectionRange(start, end);
+        } else if (this.createTextRange) {
+            var range = this.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', end);
+            range.moveStart('character', start);
+            range.select();
+        }
+    });
+};
+
+/**
+ * Функция возвращает значение GET-переменной
+ * @param name
+ * @returns {string}
+ */
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+/**
+ * DETECT IE OLD VERSION
+ */
+var oldIE = false;
+(function ($) {
+    "use strict";
+    if ($('html').is('.ie6, .ie7, .ie8')) {
+        oldIE = true;
+    }
+}(jQuery));
+
+
+/**
+ * Функция возвращает максимальное значение переданного массива
+ * @param array
+ * @returns {number}
+ */
+function maxArrayValue(array) {
+    return Math.max.apply(Math, array);
+}
+
+
+/**
+ * Аналог функции на php in_array()
+ * @param needle
+ * @param haystack
+ * @param strict
+ * @returns {boolean}
+ */
+function in_array(needle, haystack, strict) {
+    var found = false, key, strict = !!strict;
+    for (key in haystack) {
+        if ((strict && haystack[key] === needle) || (!strict && haystack[key] == needle)) {
+            found = true;
+            break;
+        }
+    }
+    return found;
+}
+
+
+/**
+ * Удаление класса средствами javascript
+ * @param className
+ */
+function removeClass(className) {
+    // convert the result to an Array object
+    var els = Array.prototype.slice.call(
+        document.getElementsByClassName(className)
+    );
+    for (var i = 0, l = els.length; i < l; i++) {
+        var el = els[i];
+        el.className = el.className.replace(
+            new RegExp('(^|\\s+)' + className + '(\\s+|$)', 'g'),
+            '$1'
+        );
+    }
+}
---
+++ library.php
@@ -0,0 +1,255 @@
+<?
+/**
+ * Вариант функции strpos(), где искомое значение может быть массивом
+ *
+ * @param $haystack
+ * @param $needles
+ * @return int
+ */
+function strpos_array($haystack, $needles) {
+    if ( is_array($needles) ) {
+        foreach ($needles as $str) {
+            if ( is_array($str) ) {
+                $pos = strpos_array($haystack, $str);
+            } else {
+                $pos = strpos($haystack, $str);
+            }
+            if ($pos !== FALSE) {
+                return $pos;
+            }
+        }
+    } else {
+        return strpos($haystack, $needles);
+    }
+}
+
+/**
+ * Фильтр убирает подрят идущие символы в переданной строке
+ *
+ * @param string $string фильтруемая строка
+ * @param string $symbol убираемый повторяющийся символ
+ * @param integer $count сколько повторений подрят убирать
+ * @return string
+ **/
+function removeRepeatingSymbols($string, $symbol = " ", $count = 2) {
+    $v = $string;
+
+    $ss = "";
+    for($i=0;$i<$count;$i++) {
+        $ss .= $symbol;
+    }
+
+    $string = str_replace($ss, $symbol, $string);
+    if($v != $string) {
+        return removeRepeatingSymbols($string, $symbol, $count);
+    } else {
+        return $string;
+    }
+}
+
+/**
+ * Функция подготавливает строку к адресной. Транслитерирует и убирает всё лишнее
+ *
+ * @relation function removeRepeatingSymbols()
+ * @param $string
+ * @return mixed|string
+ */
+function strToAliasUrl($string) {
+
+    // преобразуем специальные HTML-сущности в соответствующие символы
+    $string = htmlentities($string, ENT_QUOTES, "UTF-8");
+    $a = array('&Agrave;'=>'A','&#192;'=>'A','&Aacute;'=>'A','&#193;'=>'A','&Acirc;'=>'A','&#194;'=>'A','&Atilde;'=>'A','&#195;'=>'A','&Auml;'=>'A','&#196;'=>'A','&Aring;'=>'A','&#197;'=>'A','&AElig;'=>'AE','&#198;'=>'AE','&Ccedil;'=>'C','&#199;'=>'C','&Yacute;'=>'Y','&#221;'=>'Y','&ETH;'=>'D','&#208;'=>'D','&Ntilde;'=>'N','&#209;'=>'N','&Egrave;'=>'E','&#200;'=>'E','&Eacute;'=>'E','&#201;'=>'E','&Ecirc;'=>'E','&#202;'=>'E','&Euml;'=>'E','&#203;'=>'E','&Igrave;'=>'I','&#204;'=>'I','&Iacute;'=>'I','&#205;'=>'I','&Icirc;'=>'I','&#206;'=>'I','&Iuml;'=>'I','&#207;'=>'I','&Ograve;'=>'O','&#210;'=>'O','&Oacute;'=>'O','&#211;'=>'O','&Ocirc;'=>'O','&#212;'=>'O','&Otilde;'=>'O','&#213;'=>'O','&Ouml;'=>'O','&#214;'=>'O','&Ugrave;'=>'U','&#217;'=>'U','&Uacute;'=>'U','&#218;'=>'U','&Ucirc;'=>'U','&#219;'=>'U','&Uuml;'=>'U','&#220;'=>'U','&agrave;'=>'a','&#224;'=>'a','&aacute;'=>'a','&##225;'=>'a','&acirc;'=>'a','&##226;'=>'a','&atilde;'=>'a','&#227;'=>'a','&auml;'=>'a','&#228;'=>'a','&aring;'=>'a','&#229;'=>'a','&aelig;'=>'ae','&#230;'=>'ae','&egrave;'=>'e','&#232;'=>'e','&eacute;'=>'e','&#233;'=>'e','&ecirc;'=>'e','&#234;'=>'e','&euml;'=>'e','&#235;'=>'e','ё'=>'e','&igrave;'=>'i','&#236;'=>'i','&iacute;'=>'i','&#237;'=>'i','&icirc;'=>'i','&#238;'=>'i','&iuml;'=>'i','&#239;'=>'i','&ograve;'=>'o','&#242;'=>'o','&oacute;'=>'o','&#243;'=>'o','&ocirc;'=>'o','&#244;'=>'o','&otilde;'=>'o','&#245;'=>'o','&ouml;'=>'o','&#246;'=>'o','&ugrave;'=>'u','&#249;'=>'u','&uacute;'=>'u','&#250;'=>'u','&ucirc;'=>'u','&#251;'=>'u','&uuml;'=>'u','&#252;'=>'u','&yacute;'=>'y','&#253;'=>'y','&yuml;'=>'y','&#255;'=>'y','&ntilde;'=>'n','&#241;'=>'n','&ccedil;'=>'c','&#231;'=>'c','&ndash;'=>'-','&#8211;'=>'-','&mdash;'=>'-','&#8212;'=>'-','&oline;'=>'-','&#8254;'=>'-');
+    $string = strtr($string, $a);
+
+    // переводим в строчные, убираем теги и пробелы/дефисы по краям
+    $string = trim(mb_strtolower(strip_tags($string), 'utf8'));
+    $string = trim($string, '-');
+
+    // удаляем прочие символы-сущности, которые по какии-либо причинам не отфильтровались выше
+    $string = preg_replace('/&[a-zA-Z0-9#]+\;/', '', $string);
+
+    // транслируем
+    $tr = array("а"=>"a","б"=>"b","в"=>"v","г"=>"g","д"=>"d","е"=>"e","ё"=>"e","ж"=>"j","з"=>"z","и"=>"i","й"=>"y","к"=>"k","л"=>"l","м"=>"m","н"=>"n","о"=>"o","п"=>"p","р"=>"r","с"=>"s","т"=>"t","у"=>"u","ф"=>"f","х"=>"h","ц"=>"ts","ч"=>"ch","ш"=>"sh","щ"=>"sch","ъ"=>"y","ы"=>"yi","ь"=>"","э"=>"e","ю"=>"yu","я"=>"ya");
+    $string = strtr($string,$tr);
+
+    // разрешенные символы, убираем те, что отсутствуют в этом списке
+    $new_name = '';
+    $mix = array('q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','0','1','2','3','4','5','6','7','8','9','-',' ');
+    for($i=0; $i<=strlen($string)-1; $i++) {
+        $j = substr($string,$i,1);
+        if (in_array($j, $mix)) {
+            $new_name .= $j;
+        }
+    }
+    $string = $new_name;
+
+    // убираем подряд идущие пробелы и дефисы
+    $string = removeRepeatingSymbols($string, ' ');
+    $string = removeRepeatingSymbols($string, '-');
+
+    // заменяем пробелы на дефисы
+    $string = str_replace(array(' - ', '- ', ' -', ' ', '_', '—'), '-', $string);
+    $string = trim($string, ' ');
+    $string = trim($string, '-');
+
+    return $string;
+}
+
+/**
+ * Функция подготавливает строку к вставке для значения в мета-тег
+ * Заменяет любые кавычки на одинарные, убирает html-теги, заменяет html-сущности на норм. символы
+ *
+ * @relation function removeRepeatingSymbols()
+ * @param $string
+ * @return mixed|string
+ */
+function stringToMeta($string) {
+    // убираем html-теги
+    $string = strip_tags($string);
+
+    // преобразуем специальные HTML-сущности в соответствующие символы
+    $string = html_entity_decode($string, ENT_QUOTES, "UTF-8");
+    $a = array('&Agrave;'=>'A','&#192;'=>'A','&Aacute;'=>'A','&#193;'=>'A','&Acirc;'=>'A','&#194;'=>'A','&Atilde;'=>'A','&#195;'=>'A','&Auml;'=>'A','&#196;'=>'A','&Aring;'=>'A','&#197;'=>'A','&AElig;'=>'AE','&#198;'=>'AE','&Ccedil;'=>'C','&#199;'=>'C','&Yacute;'=>'Y','&#221;'=>'Y','&ETH;'=>'D','&#208;'=>'D','&Ntilde;'=>'N','&#209;'=>'N','&Egrave;'=>'E','&#200;'=>'E','&Eacute;'=>'E','&#201;'=>'E','&Ecirc;'=>'E','&#202;'=>'E','&Euml;'=>'E','&#203;'=>'E','&Igrave;'=>'I','&#204;'=>'I','&Iacute;'=>'I','&#205;'=>'I','&Icirc;'=>'I','&#206;'=>'I','&Iuml;'=>'I','&#207;'=>'I','&Ograve;'=>'O','&#210;'=>'O','&Oacute;'=>'O','&#211;'=>'O','&Ocirc;'=>'O','&#212;'=>'O','&Otilde;'=>'O','&#213;'=>'O','&Ouml;'=>'O','&#214;'=>'O','&Ugrave;'=>'U','&#217;'=>'U','&Uacute;'=>'U','&#218;'=>'U','&Ucirc;'=>'U','&#219;'=>'U','&Uuml;'=>'U','&#220;'=>'U','&agrave;'=>'a','&#224;'=>'a','&aacute;'=>'a','&##225;'=>'a','&acirc;'=>'a','&##226;'=>'a','&atilde;'=>'a','&#227;'=>'a','&auml;'=>'a','&#228;'=>'a','&aring;'=>'a','&#229;'=>'a','&aelig;'=>'ae','&#230;'=>'ae','&egrave;'=>'e','&#232;'=>'e','&eacute;'=>'e','&#233;'=>'e','&ecirc;'=>'e','&#234;'=>'e','&euml;'=>'e','&#235;'=>'e','ё'=>'e','&igrave;'=>'i','&#236;'=>'i','&iacute;'=>'i','&#237;'=>'i','&icirc;'=>'i','&#238;'=>'i','&iuml;'=>'i','&#239;'=>'i','&ograve;'=>'o','&#242;'=>'o','&oacute;'=>'o','&#243;'=>'o','&ocirc;'=>'o','&#244;'=>'o','&otilde;'=>'o','&#245;'=>'o','&ouml;'=>'o','&#246;'=>'o','&ugrave;'=>'u','&#249;'=>'u','&uacute;'=>'u','&#250;'=>'u','&ucirc;'=>'u','&#251;'=>'u','&uuml;'=>'u','&#252;'=>'u','&yacute;'=>'y','&#253;'=>'y','&yuml;'=>'y','&#255;'=>'y','&ntilde;'=>'n','&#241;'=>'n','&ccedil;'=>'c','&#231;'=>'c','&ndash;'=>'-','&#8211;'=>'-','&mdash;'=>'-','&#8212;'=>'-','&oline;'=>'-','&#8254;'=>'-');
+    $string = strtr($string, $a);
+
+    // заменяем любые кавычки на одинарные
+    $arQuote = array(
+        '"', '&quot;', '&#34;', // двойная кавычка
+        "«", '»', '&laquo;', '&raquo;', '&#171;', '&#187;', // типографская кавычка
+        '&prime;', '&#8242;', '&Prime;', '&#8243;',  // штрих (двойной и одинарный)
+        '&lsquo;', '&#8216;', '&rsquo;', '&#8217;', '&sbquo;', '&#8218;', // одиночная кавычка
+        '&ldquo;', '&#8220;', '&rdquo;', '&#8221;', '&bdquo;', '&#8222;', // кавычка-лапка
+        '&#10075;', '&#10076;', '&#10077;', '&#10078;' // английские кавычи
+    );
+    $string = str_replace($arQuote, "'", $string);
+
+    // убираем подряд идущие пробелы и дефисы
+    $string = removeRepeatingSymbols($string, ' ');
+    $string = removeRepeatingSymbols($string, '-');
+    $string = trim($string, ' ');
+    $string = trim($string, '-');
+
+    // удаляем прочие символы-сущности, которые по какии-либо причинам не отфильтровались выше
+    $string = preg_replace('/&[a-zA-Z0-9#]+\;/', '', $string);
+
+    return $string;
+}
+
+/**
+ * Функция преобразует первый символ строки в верхний регистр
+ * 
+ * @param $string
+ * @param string $encoding
+ * @return string
+ */
+function stringToUpperFirst($string, $encoding='UTF-8') {
+    $string = mb_ereg_replace('^[\ ]+', '', $string);
+    $string = mb_strtoupper(mb_substr($string, 0, 1, $encoding), $encoding).
+        mb_substr($string, 1, mb_strlen($string), $encoding);
+    return $string;
+}
+
+/**
+ * Обрамляем абзацы текста в тег <p>
+ *
+ * @param $string
+ * @return string
+ */
+function paragraphToTag($string) {
+    $res = '';
+    $a = explode("\n", $string);
+    foreach($a as $val) {
+        $val = str_replace("\r", "", $val);
+        if (strlen($val) > 1) {
+            $res .= '<p>'.$val.'</p>'."\n";
+        }
+    }
+    return $res;
+}
+
+/**
+ * Генерация кода из цифр и букв в нижнем и верхнем регистрах
+ *
+ * @param integer $len
+ * @return string
+ */
+function randHardCode($len) {
+    $arSymbols = array(
+        "a","b","c","d","e","f","g","h","j","k","l","m","n","p","q","r","s","t","u","v","w","x","y","z",
+        "A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z",
+        0,1,2,3,4,5,6,7,8,9
+    );
+    $code = '';
+    for($i = 0; $i < $len; $i++) {
+        $a = rand(0, 57);
+        $code = $code.$arSymbols[$a];
+    }
+    return $code;
+}
+
+/**
+ * Генерация кода из цифр и букв нижнего регистра
+ *
+ * @param integer $len
+ * @return string
+ */
+function randEasyCode($len) {
+    $arSymbols = array("a","b","c","d","e","f","g","h","j","k","l","m","n","p","q","r","s","t","u","v","w","x","y","z",0,1,2,3,4,5,6,7,8,9);
+    $code = '';
+    for($i = 0; $i < $len; $i++) {
+        $a = rand(0, 33);
+        $code = $code.$arSymbols[$a];
+    }
+    return $code;
+}
+
+/**
+ * Class Filter_BBCode
+ */
+class Filter_BBCode {
+    protected $codes = array(
+        "\n"    => '<br />',
+        '[b]'   => '<strong>',
+        '[/b]'  => '</strong>',
+        '[i]'   => '<i>',
+        '[/i]'  => '</i>',
+        '[u]'   => '<u>',
+        '[/u]'  => '</u>'
+    );
+
+    public function filter($string) {
+        $v = str_ireplace(array_flip($this->codes), $this->codes, $string);
+
+        // links
+        $p = '/(http:\/\/|https:\/\/)?([a-zA-Z0-9]+[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}([\/a-zA-Z0-9\-_\?%&=]+)?)/';
+        $r = '<a href="http://$2" target="_blank">$1$2</a>';
+        $v = preg_replace($p, $r, $v);
+
+        return $v;
+    }
+
+    public function Decode($string) {
+        $v = str_ireplace($this->codes, array_flip($this->codes), $string);
+        $v = preg_replace('/<a href="(.*)" target="_blank">(.*)<\/a>/', '$2', $v);
+        return $v;
+    }
+}
+
+
+/**
+ * Функция заменяет ключ массива на другой
+ *
+ * @param $key
+ * @param $new_key
+ * @param $arr
+ * @param bool $rewrite
+ * @return bool
+ */
+function arrayChangeKey($key,$new_key,&$arr,$rewrite=true){
+    if(!array_key_exists($new_key,$arr) || $rewrite){
+        $arr[$new_key]=$arr[$key];
+        unset($arr[$key]);
+        return true;
+    }
+    return false;
+}
