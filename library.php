59cf859272b7f07de6eb80b068605bd68c595a2a
d-bogdanov <denx-b@mail.ru>
2015-02-03 20:07:35 +0200

    Добавлена функция chmod_R

--- library.php
+++ library.php
@@ -284,3 +284,28 @@ function normalizeLine($string) {
     // Update the file contents
     return $string;
 }
+
+
+/**
+ * Рекурсвная смена прав для файлов и папок. Функция взята из курса Битрикс "Администратор. Базовый"
+ *
+ * @param $path
+ */
+function chmod_R($path) {
+    $BX_FILE_PERMISSIONS = 0644;
+    $BX_DIR_PERMISSIONS = 0755;
+
+    $handle = opendir($path);
+    while ( false !== ($file = readdir($handle)) ) {
+        if ( ($file !== ".") && ($file !== "..") ) {
+            if ( is_file($path."/".$file) ) {
+                chmod($path . "/" . $file, $BX_FILE_PERMISSIONS);
+            }
+            else {
+                chmod($path . "/" . $file, $BX_DIR_PERMISSIONS);
+                chmod_R($path . "/" . $file);
+            }
+        }
+    }
+    closedir($handle);
+}
